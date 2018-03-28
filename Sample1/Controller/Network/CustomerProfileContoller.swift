518c81451234ccea448b6f84486e1b3a7377ae0f
shruthi <iw02@iw02s-MacBook-Pro.local>
2018-03-28 16:30:43 +0530

    customer profile ViewController added

---
+++ Sample1/Controller/Network/CustomerProfileContoller.swift
@@ -0,0 +1,27 @@
+//
+//  CustomerProfileContoller.swift
+//  Sample1
+//
+//  Created by iw02 on 28/03/18.
+//  Copyright © 2018 iw02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomerProfileController: UIViewController {
+    
+    
+    @IBOutlet weak var customerName: UILabel!
+    @IBOutlet weak var mobileNumber: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var pin: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func calenderButtonPressed(_ sender: Any) {
+    }
+    
+}
